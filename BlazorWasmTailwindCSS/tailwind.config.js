5e469984ec445e25433957bc470d539d57b9cc15
Juan Barahona <barahonajm@outlook.com>
2021-04-11 22:49:06 -0400

    Initial commit for blzortailwindcss template

---
+++ BlazorWasmTailwindCSS/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './Pages/**/*.razor',
+    './Shared/**/*.razor',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
