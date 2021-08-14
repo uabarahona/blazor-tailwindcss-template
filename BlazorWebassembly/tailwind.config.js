69cfa11bfb170424dbb2ee388c93b7f8c3b03fe7
Arkadiy <arkada38@outlook.com>
2021-08-14 23:53:39 +0300

    Added npm clean-install (#6)
    * Renamed sln and project files
    
    * Added package restore command via npm ci
    
    * postcss.config.js moved to package.json
    
    * Added target NodeModulesInstall
    
    * Changed npm i to npm ci for restoring node_modules folder
    
    * Adding final MSBUILD correction | revert postcss.config.js
    
    Co-authored-by: Juan Barahona <barahonajm@outlook.com>

---
+++ BlazorWebassembly/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    mode: 'jit',
+    purge: [
+        './Pages/Counter.razor',
+        './Pages/FetchData.razor',
+        './Pages/Index.razor',
+        './Shared/MainLayout.razor',
+        './Shared/NavMenu.razor',
+        './Shared/SurveyPrompt.razor'
+    ],
+    darkMode: false,
+    theme: {
+        extend: {},
+    },
+    variants: {},
+    plugins: []
+}
