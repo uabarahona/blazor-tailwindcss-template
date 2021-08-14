6a143513e6ddf4a275c8c3ac26d7aad8fa76548b
Juan Barahona <barahonajm@outlook.com>
2021-08-14 16:51:58 -0400

    Adding final MSBUILD correction | revert postcss.config.js

--- BlazorWebassembly/tailwind.config.js
+++ BlazorWebassembly/tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
     mode: 'jit',
     purge: [
-        './Pages/*.razor'
+        './Pages/Counter.razor',
+        './Pages/FetchData.razor',
+        './Pages/Index.razor',
+        './Shared/MainLayout.razor',
+        './Shared/NavMenu.razor',
+        './Shared/SurveyPrompt.razor'
     ],
     darkMode: false,
     theme: {
